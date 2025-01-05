87c0c7ef80f4988d8cb351ae672e4bb0492a721b
n4ryn <vinaykumar.cs19@aryacollege.in>
2025-01-06 02:34:21 +0530

    feat: Update logo url

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,13 @@ const Header = () => {
   return (
     <div className="flex justify-between items-cente mb-16">
       <div className="flex items-center gap-4">
-        <img src="/vinay.webp" alt="Logo" className="h-12 w-12 rounded-full" />
+        <a href="/" className="cursor-pointer">
+          <img
+            src="/vinay.webp"
+            alt="Logo"
+            className="h-12 w-12 rounded-full"
+          />
+        </a>
         <div className="flex flex-col">
           <p className="text-text-primary font-bold text-md">Vinay Kumar</p>
           <p className="text-text-secondary font-normal text-sm">
